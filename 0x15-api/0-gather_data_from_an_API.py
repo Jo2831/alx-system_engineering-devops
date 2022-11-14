3005e58f6ed4bab134f596887c450066e7021223
Jo2831 <jo2831@github.com>
2022-11-14 07:17:16 -0900

    api_tsk0_u

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-'''
-Python script that returns information using REST API
-'''
+''' makes a get request to a REST API '''
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    if len(argv) > 1:
-        user = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-        req = requests.get("{}users/{}".format(url, user))
-        name = req.json().get("name")
-        if name is not None:
-            jreq = requests.get(
-                "{}todos?userId={}".format(
-                    url, user)).json()
-            alltsk = len(jreq)
-            completedtsk = []
-            for t in jreq:
-                if t.get("completed") is True:
-                    completedtsk.append(t)
-            count = len(completedtsk)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, count, alltsk))
-            for title in completedtsk:
-                print("\t {}".format(title.get("title")))
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_data = requests.get(url + 'users/{}'.format(argv[1])).json()
+    user_tasks = requests.get(url + 'todos', params={'userId': argv[1]}).json()
+    completed_tasks = [task.get('title')
+                       for task in user_tasks if task.get('completed') is True]
+    print('Employee {} is done with tasks({}/{}):'
+          .format(user_data.get('name'),
+                  len(completed_tasks), len(user_tasks)))
+    [print('\t {}'.format(title)) for title in completed_tasks]
